f6ab4c375d6ebbc1725132c4b315ddb81a5849bc
Johanna Heller <johanna.heller@student.uibk.ac.at>
2021-03-25 16:39:12 +0100

    stop objekt

--- nz/main.js
+++ nz/main.js
@@ -1,13 +1,21 @@
-
-const map = L.map("map", {
-    center: [-38.137778, 176.251389],
+let stop = {
+    nr: 22,
+    name: "Rotorua",
+    lat: -38.137778,
+    lng: 176.251389,
+    user: "jo3093",
+    wikipedia: "https://en.wikipedia.org/wiki/Champagne_Pool"
+  }
+  
+  const map = L.map("map", {
+    center: [ stop.lat, stop.lng ],
     zoom: 13,
     layers: [
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
     ]
   });
-
-  let mrk = L.marker([-38.137778, 176.251389]).addTo(map);
+  
+  let mrk = L.marker([ stop.lat, stop.lng ]).addTo(map);
   mrk.bindPopup('Rotorua').openPopup();
-
-  console.log(document.querySelector('#map'));
+  
+  console.log(document.querySelector("#map"));
