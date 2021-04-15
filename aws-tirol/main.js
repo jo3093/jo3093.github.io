4df77a5dc0981348ed596281ba12eb61bbd7389f
Johanna Heller <johanna.heller@student.uibk.ac.at>
2021-04-15 16:17:25 +0200

    Basemaps + LayerControl

---
+++ aws-tirol/main.js
@@ -0,0 +1,20 @@
+
+let basemapGray = L.tileLayer.provider("BasemapAT.grau");
+
+let map = L.map("map", {
+    center: [47, 11],
+    zoom: 9,
+    layers: [
+        basemapGray
+    ]
+});
+
+let layerControl = L.control.layers({
+    "BasemapAT.grau": basemapGray,
+    "BasemapAT.orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+    "BasemapAT.surface": L.tileLayer.provider("BasemapAT.surface"),
+    "BasemapAT.overlay+ortho": L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ])  
+}).addTo(map);
